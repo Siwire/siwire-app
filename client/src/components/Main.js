f46283ec0369811cd2850564ed609db895d7c939
Siwire <kholyavo.ivan@gmail.com>
2021-05-23 14:09:59 +0300

    header ui

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,7 +1,11 @@
 import Grid from '@material-ui/core/Grid';
+import Header from './Header'
+import Panel from './Panel';
 
-export default function Main(){
+
+export default function Main() {
     return <Grid>
-
+        <Header />
+        <Panel />
     </Grid>
 }
